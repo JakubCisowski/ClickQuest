13cb007a99b2a6e9f85821cce11a038def5a09af
JakubCisowski <jakub.cisowski1@gmail.com>
2020-12-06 17:00:20 +0100

    Add crafting artifacts from materials using recipes in blacksmith.

--- Pages/BlacksmithPage.xaml.cs
+++ Pages/BlacksmithPage.xaml.cs
@@ -17,6 +17,7 @@ namespace ClickQuest.Pages
 		public void UpdateBlacksmith()
 		{
 			ItemsListViewMelt.ItemsSource = User.Instance.Materials;
+			ItemsListViewCraft.ItemsSource = User.Instance.Recipes;
 
 			ItemsListViewMelt.Items.Refresh();
 		}
@@ -34,6 +35,44 @@ namespace ClickQuest.Pages
 			// Add an ingot of that rarity
 			var ingot = Account.User.Instance.Ingots.Where(x => x.Rarity == material.Rarity).FirstOrDefault();
 			ingot.Quantity++;
+
+			EquipmentWindow.Instance.UpdateEquipment();
+			UpdateBlacksmith();
+		}
+
+		private void CraftButton_Click(object sender, RoutedEventArgs e)
+		{
+			var b = sender as Button;
+			var recipe = b.CommandParameter as Recipe;
+
+			// Check if user has required materials to craft.
+			foreach (var pair in recipe.MaterialIds)
+			{
+				var material = User.Instance.Materials.FirstOrDefault(x=>x.Id==pair.Key);
+				if (! (material!=null && material.Quantity>=pair.Value))
+				{
+					// Error - no materials.
+					return;
+				}
+			}
+			
+			
+			// If he has, remove them.
+			foreach (var pair in recipe.MaterialIds)
+			{
+				var material = User.Instance.Materials.FirstOrDefault(x=>x.Id==pair.Key);
+				if (material!=null && material.Quantity>=pair.Value)
+				{
+					for(int i=0; i<pair.Value; i++)
+					{
+						User.Instance.RemoveItem(material);
+					}
+				}
+			}
+
+			// Add artifact to equipment.
+			var artifact = Data.Database.Artifacts.FirstOrDefault(x=>x.Id == recipe.ArtifactId);
+			User.Instance.AddItem(artifact);
 
 			EquipmentWindow.Instance.UpdateEquipment();
 			UpdateBlacksmith();
