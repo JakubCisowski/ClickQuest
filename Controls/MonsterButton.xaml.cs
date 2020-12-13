94ee1f9ba6a904351de77829a33e31bab2d4f80d
JakubCisowski <jakub.cisowski1@gmail.com>
2020-12-13 16:45:06 +0100

    Add poison.

--- Controls/MonsterButton.xaml.cs
+++ Controls/MonsterButton.xaml.cs
@@ -8,6 +8,7 @@ using System.ComponentModel;
 using System.Runtime.CompilerServices;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Threading;
 
 namespace ClickQuest.Controls
 {
@@ -29,6 +30,8 @@ namespace ClickQuest.Controls
 		private Region _region;
 		private Random _rng = new Random();
 		private RegionPage _regionPage;
+		private DispatcherTimer _poisonTimer;
+		private int _poisonTicks;
 		#endregion
 
 		#region Properties
@@ -53,6 +56,11 @@ namespace ClickQuest.Controls
 			// Set Region and RegionPage to which the monster belongs.
 			_region = region;
 			_regionPage = regionPage;
+
+			// Setup Timer to tick every 0.5s.
+			_poisonTimer = new DispatcherTimer();
+			_poisonTimer.Interval = new TimeSpan(0,0,0,0,500);
+			_poisonTimer.Tick+=PoisonTimer_Tick;
 
 			SpawnMonster();
 		}
@@ -97,10 +105,30 @@ namespace ClickQuest.Controls
 			_regionPage.TestRewardsBlock.Text = "Loot: " + _monster.Loot[i].Item.Name + ", Exp: " + Experience.CalculateMonsterXpReward(_monster.Health);
 		}
 
+		private void CheckIfMonsterDied()
+		{
+			// If monster died - grant rewards and spawn another one.
+			if (Monster.CurrentHealth <= 0)
+			{
+				// Reset poison.
+				_poisonTimer.Stop();
+				_poisonTicks = 0;
+
+				// Grant exp and loot.
+				GrantVictoryBonuses();
+
+				// Spawn new monster.
+				SpawnMonster();
+			}
+		}
+
 		#region Events
 		private void MonsterButton_Click(object sender, RoutedEventArgs e)
 		{
 			// Deal damage to monster.
+			_poisonTicks = 0;
+			_poisonTimer.Start();
+
 			int damage = Account.User.Instance.CurrentHero.ClickDamage;
 			double num = _rng.Next(1, 101) / 100d;
 			if (num <= Account.User.Instance.CurrentHero.CritChance)
@@ -109,11 +137,25 @@ namespace ClickQuest.Controls
 			}
 			Monster.CurrentHealth -= damage;
 
-			// If monster died - grant rewards and spawn another one.
-			if (Monster.CurrentHealth <= 0)
+			// Check if monster is dead now.
+			CheckIfMonsterDied();
+		}
+		
+		private void PoisonTimer_Tick(object source, EventArgs e)
+		{
+			// When poison ends, stop poison timer.
+			if (_poisonTicks >= 5)
 			{
-				GrantVictoryBonuses();
-				SpawnMonster();
+				_poisonTimer.Stop();
+			}
+			// Otherwise deal poison damage and check if monster died.
+			else
+			{
+				int poisonDamage = Account.User.Instance.CurrentHero.PoisonDamage;
+				_poisonTicks++;
+				Monster.CurrentHealth -= poisonDamage;
+				// Check if monster is dead now.
+				CheckIfMonsterDied();
 			}
 		}
 		#endregion
