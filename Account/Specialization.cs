cdcdfc1daa0a920835b46e84ddd8d15245124236
JakubCisowski <jakub.cisowski1@gmail.com>
2020-12-16 17:38:39 +0100

    Add specializations properties.

--- Account/Specialization.cs
+++ Account/Specialization.cs
@@ -1,9 +1,20 @@
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
 namespace ClickQuest.Account
 {
-    public partial class Specialization
+    public partial class Specialization : INotifyPropertyChanged
     {
+        #region INotifyPropertyChanged
+		public event PropertyChangedEventHandler PropertyChanged;
+		protected void OnPropertyChanged([CallerMemberName] string name = null)
+		{
+			PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+		}
+		#endregion
 
         #region Singleton
+
         private static Specialization _instance;
 
         public static Specialization Instance
@@ -12,12 +23,13 @@ namespace ClickQuest.Account
             {
                 if (_instance is null)
                 {
-                    _instance=new Specialization();
+                    _instance = new Specialization();
                 }
                 return _instance;
             }
         }
-        #endregion
+
+        #endregion Singleton
 
         #region Private Fields
 
@@ -43,7 +55,245 @@ namespace ClickQuest.Account
 
         private int specKillingAmount;
         private int specKillingThreshold;
-        private int specKillingBuff;   
+        private int specKillingBuff;
+
+        #endregion Private Fields
+
+        #region Properties
+
+        public int SpecBuyingAmount
+        {
+            get
+            {
+                return specBuyingAmount;
+            }
+            set
+            {
+                specBuyingAmount = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public int SpecBuyingThreshold
+        {
+            get
+            {
+                return specBuyingThreshold;
+            }
+            set
+            {
+                specBuyingThreshold = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public int SpecBuyingBuff
+        {
+            get
+            {
+                return specBuyingBuff;
+            }
+            set
+            {
+                specBuyingBuff = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public int SpecMeltingAmount
+        {
+            get
+            {
+                return specMeltingAmount;
+            }
+            set
+            {
+                specMeltingAmount = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public int SpecMeltingThreshold
+        {
+            get
+            {
+                return specMeltingThreshold;
+            }
+            set
+            {
+                specMeltingThreshold = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public int SpecMeltingBuff
+        {
+            get
+            {
+                return specMeltingBuff;
+            }
+            set
+            {
+                specMeltingBuff = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public int SpecCraftingAmount
+        {
+            get
+            {
+                return specCraftingAmount;
+            }
+            set
+            {
+                specCraftingAmount = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public int SpecCraftingThreshold
+        {
+            get
+            {
+                return specCraftingThreshold;
+            }
+            set
+            {
+                specCraftingThreshold = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public int SpecCraftingBuff
+        {
+            get
+            {
+                return specCraftingBuff;
+            }
+            set
+            {
+                specCraftingBuff = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public int SpecQuestingAmount
+        {
+            get
+            {
+                return specQuestingAmount;
+            }
+            set
+            {
+                specQuestingAmount = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public int SpecQuestingThreshold
+        {
+            get
+            {
+                return specQuestingThreshold;
+            }
+            set
+            {
+                specQuestingThreshold = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public int SpecQuestingBuff
+        {
+            get
+            {
+                return specQuestingBuff;
+            }
+            set
+            {
+                specQuestingBuff = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public int SpecBlessingAmount
+        {
+            get
+            {
+                return specBlessingAmount;
+            }
+            set
+            {
+                specBlessingAmount = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public int SpecBlessingThreshold
+        {
+            get
+            {
+                return specBlessingThreshold;
+            }
+            set
+            {
+                specBlessingThreshold = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public int SpecBlessingBuff
+        {
+            get
+            {
+                return specBlessingBuff;
+            }
+            set
+            {
+                specBlessingBuff = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public int SpecKillingAmount
+        {
+            get
+            {
+                return specKillingAmount;
+            }
+            set
+            {
+                specKillingAmount = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public int SpecKillingThreshold
+        {
+            get
+            {
+                return specKillingThreshold;
+            }
+            set
+            {
+                specKillingThreshold = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public int SpecKillingBuff
+        {
+            get
+            {
+                return specKillingBuff;
+            }
+            set
+            {
+                specKillingBuff = value;
+                OnPropertyChanged();
+            }
+        }
 
         #endregion
     }
