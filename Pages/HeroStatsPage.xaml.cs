370daa37201c6d114249ec5abc1817455bf807a0
JakubCisowski <jakub.cisowski1@gmail.com>
2020-12-19 19:21:17 +0100

    Display specializations in hero stats panel.

--- Pages/HeroStatsPage.xaml.cs
+++ Pages/HeroStatsPage.xaml.cs
@@ -18,6 +18,7 @@ namespace ClickQuest.Pages
 
 			GenerateIngots();
 			GenerateGold();
+			GenerateSpecializations();
 		}
 
 		private void ShowEquipmentButton_Click(object sender, RoutedEventArgs e)
@@ -64,5 +65,162 @@ namespace ClickQuest.Pages
 				}
 			}
 		}
+
+		private void GenerateSpecializations()
+		{
+			SpecializationsPanel.Children.Clear();
+			
+			var sectionBlock = new TextBlock()
+			{
+				Name = "Specializations",
+				Text="Specializations:"
+			};
+			SpecializationsPanel.Children.Add(sectionBlock);
+
+
+			{
+				var block = new TextBlock()
+				{
+					Name = "SpecBuying"
+				};
+
+				Binding binding = new Binding("SpecBuyingAmount");
+				binding.Source = Account.User.Instance.Specialization;
+				Binding binding2 = new Binding("SpecBuyingThreshold");
+				binding2.Source = Account.User.Instance.Specialization;
+				Binding binding3 = new Binding("SpecBuyingBuff");
+				binding3.Source = Account.User.Instance.Specialization;
+
+				MultiBinding multiBinding = new MultiBinding();
+				multiBinding.StringFormat = "Buying - Amount: {0}; Buff: {2}; Threshold: {1}";
+				multiBinding.Bindings.Add(binding);
+				multiBinding.Bindings.Add(binding2);
+				multiBinding.Bindings.Add(binding3);
+
+				block.SetBinding(TextBlock.TextProperty, multiBinding);
+
+				SpecializationsPanel.Children.Add(block);
+			}
+
+			{
+				var block = new TextBlock()
+				{
+					Name = "SpecMelting"
+				};
+
+				Binding binding = new Binding("SpecMeltingAmount");
+				binding.Source = Account.User.Instance.Specialization;
+				Binding binding2 = new Binding("SpecMeltingThreshold");
+				binding2.Source = Account.User.Instance.Specialization;
+				Binding binding3 = new Binding("SpecMeltingBuff");
+				binding3.Source = Account.User.Instance.Specialization;
+
+				MultiBinding multiBinding = new MultiBinding();
+				multiBinding.StringFormat = "Melting - Amount: {0}; Buff: {2}; Threshold: {1}";
+				multiBinding.Bindings.Add(binding);
+				multiBinding.Bindings.Add(binding2);
+				multiBinding.Bindings.Add(binding3);
+
+				block.SetBinding(TextBlock.TextProperty, multiBinding);
+
+				SpecializationsPanel.Children.Add(block);
+			}
+
+			{
+				var block = new TextBlock()
+				{
+					Name = "SpecCrafting"
+				};
+
+				Binding binding = new Binding("SpecCraftingAmount");
+				binding.Source = Account.User.Instance.Specialization;
+				Binding binding2 = new Binding("SpecCraftingThreshold");
+				binding2.Source = Account.User.Instance.Specialization;
+				Binding binding3 = new Binding("SpecCraftingBuff");
+				binding3.Source = Account.User.Instance.Specialization;
+
+				MultiBinding multiBinding = new MultiBinding();
+				multiBinding.StringFormat = "Crafting - Amount: {0}; Buff: {2}; Threshold: {1}";
+				multiBinding.Bindings.Add(binding);
+				multiBinding.Bindings.Add(binding2);
+				multiBinding.Bindings.Add(binding3);
+
+				block.SetBinding(TextBlock.TextProperty, multiBinding);
+
+				SpecializationsPanel.Children.Add(block);
+			}
+
+			{
+				var block = new TextBlock()
+				{
+					Name = "SpecQuesting"
+				};
+
+				Binding binding = new Binding("SpecQuestingAmount");
+				binding.Source = Account.User.Instance.Specialization;
+				Binding binding2 = new Binding("SpecQuestingThreshold");
+				binding2.Source = Account.User.Instance.Specialization;
+				Binding binding3 = new Binding("SpecQuestingBuff");
+				binding3.Source = Account.User.Instance.Specialization;
+
+				MultiBinding multiBinding = new MultiBinding();
+				multiBinding.StringFormat = "Questing - Amount: {0}; Buff: {2}; Threshold: {1}";
+				multiBinding.Bindings.Add(binding);
+				multiBinding.Bindings.Add(binding2);
+				multiBinding.Bindings.Add(binding3);
+
+				block.SetBinding(TextBlock.TextProperty, multiBinding);
+
+				SpecializationsPanel.Children.Add(block);
+			}
+
+			{
+				var block = new TextBlock()
+				{
+					Name = "SpecKilling"
+				};
+
+				Binding binding = new Binding("SpecKillingAmount");
+				binding.Source = Account.User.Instance.Specialization;
+				Binding binding2 = new Binding("SpecKillingThreshold");
+				binding2.Source = Account.User.Instance.Specialization;
+				Binding binding3 = new Binding("SpecKillingBuff");
+				binding3.Source = Account.User.Instance.Specialization;
+
+				MultiBinding multiBinding = new MultiBinding();
+				multiBinding.StringFormat = "Killing - Amount: {0}; Buff: {2}; Threshold: {1}";
+				multiBinding.Bindings.Add(binding);
+				multiBinding.Bindings.Add(binding2);
+				multiBinding.Bindings.Add(binding3);
+
+				block.SetBinding(TextBlock.TextProperty, multiBinding);
+
+				SpecializationsPanel.Children.Add(block);
+			}
+
+			{
+				var block = new TextBlock()
+				{
+					Name = "SpecBlessing"
+				};
+
+				Binding binding = new Binding("SpecBlessingAmount");
+				binding.Source = Account.User.Instance.Specialization;
+				Binding binding2 = new Binding("SpecBlessingThreshold");
+				binding2.Source = Account.User.Instance.Specialization;
+				Binding binding3 = new Binding("SpecBlessingBuff");
+				binding3.Source = Account.User.Instance.Specialization;
+
+				MultiBinding multiBinding = new MultiBinding();
+				multiBinding.StringFormat = "Blessing - Amount: {0}; Buff: {2}; Threshold: {1}";
+				multiBinding.Bindings.Add(binding);
+				multiBinding.Bindings.Add(binding2);
+				multiBinding.Bindings.Add(binding3);
+
+				block.SetBinding(TextBlock.TextProperty, multiBinding);
+
+				SpecializationsPanel.Children.Add(block);
+			}
+		}
+		}
 	}
-}
--- Pages/TownPage.xaml.cs
+++ Pages/TownPage.xaml.cs
@@ -57,6 +57,7 @@ namespace ClickQuest.Pages
 
 		private void ShopButton_Click(object sender, RoutedEventArgs e)
 		{
+			Account.User.Instance.Specialization.SpecBlessingAmount++;
 			(Window.GetWindow(this) as GameWindow).CurrentFrame.Navigate(Database.Pages["Shop"]);
 			(Database.Pages["Shop"] as ShopPage).UpdateShop();
 		}
