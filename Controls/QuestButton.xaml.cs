f3c9f9d8d43dce8291ba6d28ac2d89c459a97198
JakubCisowski <jakub.cisowski1@gmail.com>
2020-12-29 23:27:46 +0100

    Generate custom quest button control.

--- Controls/QuestButton.xaml.cs
+++ Controls/QuestButton.xaml.cs
@@ -1,9 +1,158 @@
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
 using System.Windows.Controls;
+using System.Windows.Data;
+using System.Linq;
+using ClickQuest.Items;
+using ClickQuest.Data;
 
 namespace ClickQuest.Controls
 {
-    public partial class QuestButton : Button
+    public partial class QuestButton : UserControl, INotifyPropertyChanged
     {
+        #region INotifyPropertyChanged
 
+		public event PropertyChangedEventHandler PropertyChanged;
+
+		protected void OnPropertyChanged([CallerMemberName] string name = null)
+		{
+			PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+		}
+
+		#endregion INotifyPropertyChanged
+
+        private Quest _quest;
+
+        public QuestButton(Quest quest)
+        {
+            InitializeComponent();
+
+            _quest=quest;
+            this.DataContext=_quest;
+            
+            GenerateRewards();
+        }
+
+        public void GenerateRewards()
+        {
+            #region Blessings
+            var counter = 0;
+            var previousId = 0;
+
+            for (int i = 0; i < _quest.RewardBlessingIds.Count; i++)
+            {
+                // If reward id stays the same (or it's first id in the list) - increment the counter.
+                if((i == 0 || previousId == _quest.RewardBlessingIds[i]) && (i != _quest.RewardBlessingIds.Count-1))
+                {
+                    counter++;
+                }
+                // New reward id / last id in the list - display reward info on the button.
+                else
+                {
+                    var block = new TextBlock()
+					{
+						Name = "RewardBlessing" + i.ToString()
+					};
+
+                    block.Text=$"{counter}x {Database.Blessings.FirstOrDefault(x=>x.Id==_quest.RewardBlessingIds[i]).Name}";
+                    QuestRewardsPanel.Children.Add(block);
+
+                    counter = 0;
+                }
+                
+                previousId = _quest.RewardBlessingIds[i];
+            }
+            #endregion
+
+            #region Materials
+
+            counter = 0;
+            previousId = 0;
+
+            for (int i = 0; i < _quest.RewardMaterialIds.Count; i++)
+            {
+                // If reward id stays the same (or it's first id in the list) - increment the counter.
+                if((i == 0 || previousId == _quest.RewardMaterialIds[i]) && (i != _quest.RewardMaterialIds.Count-1))
+                {
+                    counter++;
+                }
+                // New reward id / last id in the list - display reward info on the button.
+                else
+                {
+                    var block = new TextBlock()
+					{
+						Name = "RewardNaterial" + i.ToString()
+					};
+
+                    block.Text=$"{counter}x {Database.Materials.FirstOrDefault(x=>x.Id==_quest.RewardMaterialIds[i]).Name}";
+                    QuestRewardsPanel.Children.Add(block);
+
+                    counter = 0;
+                }
+                
+                previousId = _quest.RewardMaterialIds[i];
+            }
+            #endregion
+
+            #region Recipes
+
+            counter = 0;
+            previousId = 0;
+
+            for (int i = 0; i < _quest.RewardRecipeIds.Count; i++)
+            {
+                // If reward id stays the same (or it's first id in the list) - increment the counter.
+                if((i == 0 || previousId == _quest.RewardRecipeIds[i]) && (i != _quest.RewardRecipeIds.Count-1))
+                {
+                    counter++;
+                }
+                // New reward id / last id in the list - display reward info on the button.
+                else
+                {
+                    var block = new TextBlock()
+					{
+						Name = "RewardRecipe" + i.ToString()
+					};
+
+                    block.Text=$"{counter}x {Database.Recipes.FirstOrDefault(x=>x.Id==_quest.RewardRecipeIds[i]).Name}";
+                    QuestRewardsPanel.Children.Add(block);
+
+                    counter = 0;
+                }
+                
+                previousId = _quest.RewardRecipeIds[i];
+            }
+            #endregion
+
+            #region Ingots
+
+            counter = 0;
+            previousId = 0;
+
+            for (int i = 0; i < _quest.RewardIngots.Count; i++)
+            {
+                // If reward id stays the same (or it's first id in the list) - increment the counter.
+                if((i == 0 || previousId == (int)_quest.RewardIngots[i]) && (i != _quest.RewardIngots.Count-1))
+                {
+                    counter++;
+                }
+                // New reward id / last id in the list - display reward info on the button.
+                else
+                {
+                    var block = new TextBlock()
+					{
+						Name = "RewardIngot" + i.ToString()
+					};
+
+                    block.Text=counter>1 ? $"{counter}x {_quest.RewardIngots[i].ToString()} Ingots" : $"{counter}x {_quest.RewardIngots[i].ToString()} Ingot";
+                    QuestRewardsPanel.Children.Add(block);
+
+                    counter = 0;
+                }
+                
+                previousId = (int)_quest.RewardIngots[i];
+            }
+            #endregion
+        }
     }
 }
