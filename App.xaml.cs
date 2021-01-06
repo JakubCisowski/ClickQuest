75b07051408fb55c255cf4d291c8ec6fa9b2731d
JakubCisowski <jakub.cisowski1@gmail.com>
2021-01-06 19:28:34 +0100

    Seed dungeon keys in user database.

--- App.xaml.cs
+++ App.xaml.cs
@@ -15,19 +15,33 @@ namespace ClickQuest
         {
             using (var db = new UserContext())
             {
-                // Ensure the database exists.
+                // Ensure the database exists - if not, create it.
                 db.Database.EnsureCreated();
 
-                var user = db.Users.Include(x => x.Ingots).FirstOrDefault();
+                var user = db.Users.Include(x => x.Ingots).Include(x=>x.DungeonKeys).FirstOrDefault();
+
+                // The seeding has to be done here, because otherwise we would need to set up relationships manually (using foreign keys, etc.)
 
                 // If there are no ingots in the database, add them (seed).
                 if (user.Ingots.Count() == 0)
                 {
                     var rarities = Enum.GetValues(typeof(Rarity));
-                    var ingots = new List<Ingot>();
                     for (int i = 0; i < rarities.GetLength(0); i++)
                     {
                         user.Ingots.Add(new Ingot((Rarity)rarities.GetValue(i), 0));
+                    }
+
+                    db.Users.Update(user);
+                    db.SaveChanges();
+                }
+
+                // If there are no dungeon keys in the database, add them (seed).
+                if (user.DungeonKeys.Count() == 0)
+                {
+                    var rarities = Enum.GetValues(typeof(Rarity));
+                    for (int i = 0; i < rarities.GetLength(0); i++)
+                    {
+                        user.DungeonKeys.Add(new DungeonKey((Rarity)rarities.GetValue(i), 0));
                     }
 
                     db.Users.Update(user);
