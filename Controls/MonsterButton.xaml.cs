d367660d4d802eef9461ca2e7182b9b6f1c2c992
JakubCisowski <jakub.cisowski1@gmail.com>
2021-01-07 18:46:43 +0100

    Add dungeon key drop chance from monsters.

--- Controls/MonsterButton.xaml.cs
+++ Controls/MonsterButton.xaml.cs
@@ -10,6 +10,8 @@ using System.Runtime.CompilerServices;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Threading;
+using System.Collections.Generic;
+using ClickQuest.Items;
 
 namespace ClickQuest.Controls
 {
@@ -105,6 +107,9 @@ namespace ClickQuest.Controls
 			// Display exp and loot for testing purposes.
 			_regionPage.TestRewardsBlock.Text = "Loot: " + _monster.Loot[i].Item.Name + ", Exp: " + Experience.CalculateMonsterXpReward(_monster.Health);
 
+			// Check if hero got dungeon key.
+			CheckForDungeonKeyDrop();
+
 			// Increase killing specialization amount (it will update buffs in setter).
 			Account.User.Instance.Specialization.SpecKillingAmount++;
 		}
@@ -123,6 +128,61 @@ namespace ClickQuest.Controls
 
 				// Spawn new monster.
 				SpawnMonster();
+			}
+		}
+		
+		private void CheckForDungeonKeyDrop()
+		{
+			var DungeonKeyRarityChances = new List <double>();
+			double EmptyLootChance = 1;
+			double DungeonKeyRarity0Chance = 0;
+			double DungeonKeyRarity1Chance = 0;
+			double DungeonKeyRarity2Chance = 0;
+			double DungeonKeyRarity3Chance = 0;
+			double DungeonKeyRarity4Chance = 0;
+			double DungeonKeyRarity5Chance = 0;
+
+			// Set dungeon key drop rates.
+			if(Monster.Health < 100)
+			{
+				EmptyLootChance = 0.05000;
+				DungeonKeyRarity0Chance = 0.8000;
+				DungeonKeyRarity1Chance = 0.1500;
+			}
+			else if(Monster.Health < 200)
+			{
+				EmptyLootChance = 0.05000;
+				DungeonKeyRarity0Chance = 0.4500;
+				DungeonKeyRarity1Chance = 0.5000;
+			}
+
+			// Set drop rates in list for randomizing algorithm.
+			// Note that index 0 is for empty loot, rarities start with index 1 up to 6.
+			DungeonKeyRarityChances.Add(EmptyLootChance);
+			DungeonKeyRarityChances.Add(DungeonKeyRarity0Chance);
+			DungeonKeyRarityChances.Add(DungeonKeyRarity1Chance);
+			DungeonKeyRarityChances.Add(DungeonKeyRarity2Chance);
+			DungeonKeyRarityChances.Add(DungeonKeyRarity3Chance);
+			DungeonKeyRarityChances.Add(DungeonKeyRarity4Chance);
+			DungeonKeyRarityChances.Add(DungeonKeyRarity5Chance);
+
+			// Check if hero got dungeon key.
+			// Randomize loot.
+			double num = _rng.Next(1, 10001) / 10000d;
+			int i = 0;
+			while (num > DungeonKeyRarityChances[i])
+			{
+				num -= DungeonKeyRarityChances[i];
+				i++;
+			}
+			// Grant dungeon key after if algorithm didn't roll empty loot.
+			if(i != 0)
+			{
+				// Add new key to account.
+				Account.User.Instance.DungeonKeys.FirstOrDefault(x => x.Rarity==(Rarity)(i-1)).Quantity++;
+
+				// Display dungeon key drop.
+				_regionPage.TestRewardsBlock.Text += $"You've got a {(Rarity)(i-1)} Dungeon Key!";
 			}
 		}
 
