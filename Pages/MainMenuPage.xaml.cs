e1e3d8e2816add8645cc0c7ae287c3c856549750
Rafał Klinowski <klinowskirafal@gmail.com>
2021-01-23 18:43:30 +0100

    Fix an issue with hero being null after returning to MainMenu.

--- Pages/MainMenuPage.xaml.cs
+++ Pages/MainMenuPage.xaml.cs
@@ -7,96 +7,96 @@ using System.Windows.Controls;
 
 namespace ClickQuest.Pages
 {
-    public partial class MainMenuPage : Page
-    {
-        public MainMenuPage()
-        {
-            InitializeComponent();
-            GenerateHeroButtons();
-        }
+	public partial class MainMenuPage : Page
+	{
+		public MainMenuPage()
+		{
+			InitializeComponent();
+			GenerateHeroButtons();
+		}
 
-        public void GenerateHeroButtons()
-        {
-            HeroesPanel.Children.Clear();
+		public void GenerateHeroButtons()
+		{
+			HeroesPanel.Children.Clear();
 
-            // For each hero:
-            for (int i = 0; i < Account.User.Instance.Heroes.Count; i++)
-            {
-                // Generate 'select hero' button.
-                var selectHeroButton = new Button()
-                {
-                    Name = "Hero" + Account.User.Instance.Heroes[i].Id,
-                    Content = Account.User.Instance.Heroes[i].Name + ", " + Account.User.Instance.Heroes[i].ThisHeroClass.ToString() + " [" + Account.User.Instance.Heroes[i].Level + " lvl]",
-                    Width = 250,
-                    Height = 50,
-                    Margin = new Thickness(5)
-                };
+			// For each hero:
+			for (int i = 0; i < Account.User.Instance.Heroes.Count; i++)
+			{
+				// Generate 'select hero' button.
+				var selectHeroButton = new Button()
+				{
+					Name = "Hero" + Account.User.Instance.Heroes[i].Id,
+					Content = Account.User.Instance.Heroes[i].Name + ", " + Account.User.Instance.Heroes[i].ThisHeroClass.ToString() + " [" + Account.User.Instance.Heroes[i].Level + " lvl]",
+					Width = 250,
+					Height = 50,
+					Margin = new Thickness(5)
+				};
 
-                // Generate 'delete hero' button.
-                var deleteHeroButton = new Button()
-                {
-                    Name = "DeleteHero" + Account.User.Instance.Heroes[i].Id,
-                    Content = "Delete " + Account.User.Instance.Heroes[i].Name + ", " + Account.User.Instance.Heroes[i].ThisHeroClass.ToString() + " [" + Account.User.Instance.Heroes[i].Level + " lvl]",
-                    Width = 150,
-                    Height = 50,
-                    Margin = new Thickness(5)
-                };
+				// Generate 'delete hero' button.
+				var deleteHeroButton = new Button()
+				{
+					Name = "DeleteHero" + Account.User.Instance.Heroes[i].Id,
+					Content = "Delete " + Account.User.Instance.Heroes[i].Name + ", " + Account.User.Instance.Heroes[i].ThisHeroClass.ToString() + " [" + Account.User.Instance.Heroes[i].Level + " lvl]",
+					Width = 150,
+					Height = 50,
+					Margin = new Thickness(5)
+				};
 
-                // Assign click events to them.
-                selectHeroButton.Click += SelectHeroButton_Click;
-                deleteHeroButton.Click += DeleteHeroButton_Click;
+				// Assign click events to them.
+				selectHeroButton.Click += SelectHeroButton_Click;
+				deleteHeroButton.Click += DeleteHeroButton_Click;
 
-                // Add them to the  heroes panel in main menu page.
-                HeroesPanel.Children.Add(selectHeroButton);
-                HeroesPanel.Children.Add(deleteHeroButton);
-            }
-        }
+				// Add them to the  heroes panel in main menu page.
+				HeroesPanel.Children.Add(selectHeroButton);
+				HeroesPanel.Children.Add(deleteHeroButton);
+			}
+		}
 
-        #region Events
+		#region Events
 
-        private void CreateHeroButton_Click(object sender, RoutedEventArgs e)
-        {
-            (Window.GetWindow(this) as GameWindow).CurrentFrame.Navigate(Database.Pages["HeroCreation"]);
-        }
+		private void CreateHeroButton_Click(object sender, RoutedEventArgs e)
+		{
+			(Window.GetWindow(this) as GameWindow).CurrentFrame.Navigate(Database.Pages["HeroCreation"]);
+		}
 
-        private void SelectHeroButton_Click(object sender, RoutedEventArgs e)
-        {
-            // Select this hero as current hero and navigate to town.
-            var id = int.Parse((sender as Button).Name.Substring(4));
-            var hero = Account.User.Instance.Heroes.Where(x => x.Id == id).FirstOrDefault();
-            Account.User.Instance.CurrentHero = hero;
-            // Refresh hero stats panel info.
-            hero.ExperienceToNextLvl = Heroes.Experience.CalculateXpToNextLvl(hero);
+		private void SelectHeroButton_Click(object sender, RoutedEventArgs e)
+		{
+			// Select this hero as current hero and navigate to town.
+			var id = int.Parse((sender as Button).Name.Substring(4));
+			var hero = Account.User.Instance.Heroes.Where(x => x.Id == id).FirstOrDefault();
+			Account.User.Instance.CurrentHero = hero;
+			// Refresh hero stats panel info.
+			hero.ExperienceToNextLvl = Heroes.Experience.CalculateXpToNextLvl(hero);
 
-            // Resume quests for the selected hero.
-            Account.User.Instance.CurrentHero.Quests.FirstOrDefault(x => x.EndDate != default(DateTime))?.StartQuest();
+			// Resume quests for the selected hero.
+			Account.User.Instance.CurrentHero.Quests.FirstOrDefault(x => x.EndDate != default(DateTime))?.StartQuest();
 
-            // Resume blessings on this account.
-            Blessing.ResumeBlessings();
+			// Resume blessings on this account.
+			Blessing.ResumeBlessings();
 
-            Data.Database.RefreshPages();
-            (Window.GetWindow(this) as GameWindow).CurrentFrame.Navigate(Data.Database.Pages["Town"]);
-        }
+			Data.Database.RefreshPages();
+			(Window.GetWindow(this) as GameWindow).CurrentFrame.Navigate(Data.Database.Pages["Town"]);
+		}
 
-        private void DeleteHeroButton_Click(object sender, RoutedEventArgs e)
-        {
-            var id = int.Parse((sender as Button).Name.Substring(10));
-            var hero = Account.User.Instance.Heroes.Where(x => x.Id == id).FirstOrDefault();
+		private void DeleteHeroButton_Click(object sender, RoutedEventArgs e)
+		{
+			var id = int.Parse((sender as Button).Name.Substring(10));
+			var hero = Account.User.Instance.Heroes.Where(x => x.Id == id).FirstOrDefault();
 
-            // Remove the hero from User and Database.
-            Account.User.Instance.Heroes.Remove(hero);
-            Entity.EntityOperations.RemoveHero(hero);
+			// Remove the hero from User and Database.
+			Account.User.Instance.Heroes.Remove(hero);
+			Entity.EntityOperations.RemoveHero(hero);
 
-            GenerateHeroButtons();
-        }
+			GenerateHeroButtons();
+		}
 
-        private void ResetProgressButton_Click(object sender, RoutedEventArgs e)
-        {
-            // Reset all progress - only account isn't removed.
-            Entity.EntityOperations.ResetProgress();
-            GenerateHeroButtons();
-        }
+		private void ResetProgressButton_Click(object sender, RoutedEventArgs e)
+		{
+			// Reset all progress - only account isn't removed.
+			Entity.EntityOperations.ResetProgress();
+			GenerateHeroButtons();
+		}
 
-        #endregion Events
-    }
+		#endregion Events
+	}
 }
