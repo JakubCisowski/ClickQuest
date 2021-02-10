0e45e1c4bb51fcba39b4389698bcb294c78306d3
JakubCisowski <jakub.cisowski1@gmail.com>
2021-02-10 15:29:00 +0100

    Add equipment tab control.

---
+++ Pages/EquipmentPage.xaml.cs
@@ -0,0 +1,14 @@
+using ClickQuest.Account;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace ClickQuest.Pages
+{
+    public partial class EquipmentPage : Page
+    {
+        public EquipmentPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
