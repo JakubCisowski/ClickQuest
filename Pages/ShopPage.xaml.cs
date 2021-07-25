a0c0b747592bdf3d2a474e50257bde865c246cbd
JakubCisowski <jakub.cisowski1@gmail.com>
2021-07-25 14:06:55 +0200

    Refactor ShopPage.xaml.cs

--- Pages/ShopPage.xaml.cs
+++ Pages/ShopPage.xaml.cs
@@ -25,7 +25,6 @@ namespace ClickQuest.Pages
 			ItemsListViewSellMaterials.ItemsSource = User.Instance.CurrentHero?.Materials;
 			ItemsListViewSellRecipes.ItemsSource = User.Instance.CurrentHero?.Recipes;
 
-			// Calculate shop offer size according to specialization bonus (base bonus: 5).
 			if (User.Instance.CurrentHero != null)
 			{
 				ItemsListViewBuy.ItemsSource = GetShopOfferAsRecipes();
@@ -60,13 +59,9 @@ namespace ClickQuest.Pages
 			var b = sender as Button;
 			var recipe = b.CommandParameter as Recipe;
 
-			// Check if user has enough gold.
 			if (User.Instance.Gold >= recipe.Value)
 			{
-				// Show buy alert.
 				var result = AlertBox.Show($"Are you sure you want to buy {recipe.Name} for {recipe.Value} gold?", MessageBoxButton.YesNo);
-
-				// If user clicked cancel on buy alert - return.
 				if (result == MessageBoxResult.Cancel)
 				{
 					return;
@@ -75,18 +70,13 @@ namespace ClickQuest.Pages
 				recipe.AddItem();
 				User.Instance.Gold -= recipe.Value;
 
-				(GameData.Pages["Shop"] as ShopPage).EquipmentFrame.Refresh();
-				
-				// Refresh stats frame (for specialization update).
-				(GameData.Pages["Shop"] as ShopPage).StatsFrame.Refresh();
-				UpdateShop();
-
-				// Increase Specialization Buying amount.
 				User.Instance.CurrentHero.Specialization.SpecializationAmounts[SpecializationType.Buying]++;
+
+				InterfaceController.RefreshStatsAndEquipmentPanelsOnPage(GameData.Pages["Shop"]);
+				UpdateShop();
 			}
 			else
 			{
-				// Display an error.
 				AlertBox.Show($"You do not have enough gold to buy this item.\nIt costs {recipe.Value} gold.\nYou can get more gold by completing quests and selling loot from monsters and bosses.", MessageBoxButton.OK);
 			}
 		}
