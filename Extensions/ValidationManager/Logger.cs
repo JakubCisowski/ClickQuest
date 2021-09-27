85ec7ae1c7f207ac0715d54c4a288ae658daac43
JakubCisowski <jakub.cisowski1@gmail.com>
2021-09-27 15:06:38 +0200

    Add Logs/ directory to gitignore. Remove unnecessary icon file.

--- Extensions/ValidationManager/Logger.cs
+++ Extensions/ValidationManager/Logger.cs
@@ -3,16 +3,16 @@ using System.IO;
 
 namespace ClickQuest.Extensions.ValidationManager
 {
-	public static class Logger
-	{
-		public static DateTime SessionStartDate = DateTime.Now;
+    public static class Logger
+    {
+        public static DateTime SessionStartDate = DateTime.Now;
 
-		public static void Log(string log)
-		{
-			// Log bugs in specified format.
-			using var writer = new StreamWriter(Path.Combine("C:/Programowanie/ClickQuest", "Logs", "Logs " + SessionStartDate.ToString("dd-MM-yyyy-HH-mm-ss") + ".txt"), true);
+        public static void Log(string log)
+        {
+            // Log bugs in specified format.
+            using var writer = new StreamWriter(Path.Combine("C:/Programowanie/Fun/ClickQuest", "Logs", "Logs " + SessionStartDate.ToString("dd-MM-yyyy-HH-mm-ss") + ".txt"), true);
 
-			writer.WriteLine(log);
-		}
-	}
+            writer.WriteLine(log);
+        }
+    }
 }
