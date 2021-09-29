df09d352ecb18ecded9cee802842515a9337436b
Rafał Klinowski <klinowskirafal@gmail.com>
2021-09-29 13:45:26 +0200

    Restructure solution

---
+++ ClickQuest.Game/Extensions/ValidationManager/Logger.cs
@@ -0,0 +1,18 @@
+using System;
+using System.IO;
+
+namespace ClickQuest.Extensions.ValidationManager
+{
+	public static class Logger
+	{
+		public static DateTime SessionStartDate = DateTime.Now;
+
+		public static void Log(string log)
+		{
+			// Log bugs in specified format.
+			using var writer = new StreamWriter(Path.Combine("C:/Programowanie/ClickQuest/ClickQuest.Game", "Logs", "Logs " + SessionStartDate.ToString("dd-MM-yyyy-HH-mm-ss") + ".txt"), true);
+
+			writer.WriteLine(log);
+		}
+	}
+}
