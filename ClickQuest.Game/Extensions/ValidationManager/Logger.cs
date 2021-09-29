47936387e2d40861d235fb122d723fbbd1e755a7
JakubCisowski <jakub.cisowski1@gmail.com>
2021-09-29 14:19:54 +0200

    Fix logger folder placement.

--- ClickQuest.Game/Extensions/ValidationManager/Logger.cs
+++ ClickQuest.Game/Extensions/ValidationManager/Logger.cs
@@ -9,8 +9,15 @@ namespace ClickQuest.Game.Extensions.ValidationManager
 
         public static void Log(string log)
         {
+            var folderPath = Path.Combine(Directory.GetParent(Environment.CurrentDirectory).Parent.Parent.ToString(), "Logs");
+
+            if (!Directory.Exists(folderPath))
+            {
+                Directory.CreateDirectory(folderPath);
+            }
+
             // Log bugs in specified format.
-            using var writer = new StreamWriter(Path.Combine("C:/Programowanie/Fun/ClickQuest/ClickQuest.Game", "Logs", "Logs " + SessionStartDate.ToString("dd-MM-yyyy-HH-mm-ss") + ".txt"), true);
+            using var writer = new StreamWriter(Path.Combine(folderPath, "Logs " + SessionStartDate.ToString("dd-MM-yyyy-HH-mm-ss") + ".txt"), true);
 
             writer.WriteLine(log);
         }
