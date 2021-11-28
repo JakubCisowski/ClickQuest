2a53ff59b42340e373703901e01d7ca2da779c29
Rafał Klinowski <klinowskirafal@gmail.com>
2021-11-28 22:12:27 +0100

    Make it so that the OnUnhandledException handler only works in Release mode - this way we can see exceptions in development easier. Add an alert for the player upon encountering an exception.

--- ClickQuest.Game/App.xaml.cs
+++ ClickQuest.Game/App.xaml.cs
@@ -6,6 +6,7 @@ using System.Windows.Threading;
 using ClickQuest.Game.Core.GameData;
 using ClickQuest.Game.Core.Player;
 using ClickQuest.Game.Data;
+using ClickQuest.Game.Extensions.Encryption;
 using ClickQuest.Game.UserInterface.Controls;
 using ClickQuest.Game.UserInterface.Pages;
 using ClickQuest.Game.UserInterface.Windows;
@@ -17,7 +18,10 @@ namespace ClickQuest.Game
 		protected void Application_Startup(object sender, StartupEventArgs e)
 		{
 			// Set crash logs output to Logs - any unhandled exceptions will be saved to a text file locally.
+			// Release only, because we want to see Exceptions in IDE during development.
+			#if RELEASE
 			Application.Current.DispatcherUnhandledException += OnUnhandledException;
+			#endif
 
 			// Set default culture for all threads for this application (affects date and string formats, e.g. periods instead of commas)
 			CultureInfo.CurrentCulture = CultureInfo.InvariantCulture;
@@ -40,18 +44,19 @@ namespace ClickQuest.Game
 			User.SessionStartDate = DateTime.Now;
 		}
 
+#if RELEASE
 		private static void OnUnhandledException(object sender, DispatcherUnhandledExceptionEventArgs args)
 		{
 			var e = args.Exception;
-
+		
 			string directoryPath = Path.Combine(Environment.CurrentDirectory, "Logs");
 			string filePath = Path.Combine(directoryPath, $"CrashLog {DateTime.Now.ToString("dd-MM-yyyy HH-mm-ss")}.txt");
-
+		
 			if (!Directory.Exists(directoryPath))
 			{
 				Directory.CreateDirectory(directoryPath);
 			}
-			
+
 			using (var writer = new StreamWriter(filePath))
 			{
 				writer.WriteLine($"Exception message: {e.Message}");
@@ -59,11 +64,12 @@ namespace ClickQuest.Game
 				writer.WriteLine($"Stack trace: {e.StackTrace}");
 				writer.WriteLine();
 				writer.WriteLine("User.json file:");
-
+		
 				//  If exception was thrown before loading User.json (Ingots.Count can't be null after loading)
 				if (User.Instance.Ingots?.Count == 0)
 				{
-					var userJson = File.ReadAllText(UserDataLoader.UserDataPath);
+					var encryptedUserJson = File.ReadAllBytes(UserDataLoader.UserDataPath);
+					var userJson = DataEncryptionController.DecryptJsonUsingAes(encryptedUserJson);
 					writer.Write(userJson);
 				}
 				else
@@ -71,6 +77,9 @@ namespace ClickQuest.Game
 					writer.Write(UserDataLoader.Save());
 				}
 			}
+			
+			AlertBox.Show("There was an error with the game. A log file has been generated in the Logs folder. To help us fix the issue, you can contribute the log file at X. We apologize for the inconvenience.\nYour data has been saved.", MessageBoxButton.OK);
 		}
+#endif
 	}
 }
