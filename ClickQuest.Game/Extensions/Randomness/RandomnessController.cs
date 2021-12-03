8975bea364a4306a864e9012fa8de40ae4692c9b
Rafał Klinowski <klinowskirafal@gmail.com>
2021-12-04 00:11:16 +0100

    Change random number generation to instead use a StandardGenerator from the Troschuetz.Random package, instead of the built in Random type (to improve random numbers distribution).

--- ClickQuest.Game/Extensions/Randomness/RandomnessController.cs
+++ ClickQuest.Game/Extensions/Randomness/RandomnessController.cs
@@ -1,9 +1,9 @@
-using System;
+using Troschuetz.Random.Generators;
 
 namespace ClickQuest.Game.Extensions.Randomness
 {
 	public static class RandomnessController
 	{
-		public static readonly Random Rng = new Random();
+		public static readonly StandardGenerator Rng = new StandardGenerator();
 	}
 }
